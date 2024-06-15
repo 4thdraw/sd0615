d6bc3718b8c4cc4796c20e4d80b5fda8c896cfac
4thdraw <4thdraw2@gmail.com>
2024-06-15 11:30:01 +0900

    멀티데이터접근하기

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-// const | let | ver
-// const 선언과 동시 값 지정 절대불변 -> 상수선언
-// let 변수 -> 같은 이름 불가
-// ver 
-
-const myname = "방보영"
-let   myaccount = 100;
-myaccount += 1100 + 2000;
-//console.log("금액확인",myaccount);
-
-myaccount +="원";
-myaccount +=5000; 
-myaccount +=5000; 
-
-//console.log("단위추가",myaccount, "데이터타입은? ",typeof myaccount);
-
-const mynavi = ["네이버","다음","구글"];
-const mynaivobj = {
-    naver : "네이버",
-    daum : "다음",
-    goggle : "구글"
+{
+    "books": [
+        {
+            "title": "The Catcher in the Rye",
+            "author": "J.D. Salinger",
+            "year_published": 1951,
+            "genres": ["Fiction", "Classics"],
+            "available": true
+        },
+        {
+            "title": "To Kill a Mockingbird",
+            "author": "Harper Lee",
+            "year_published": 1960,
+            "genres": ["Fiction", "Classics"],
+            "available": false
+        },
+        {
+            "title": "1984",
+            "author": "George Orwell",
+            "year_published": 1949,
+            "genres": ["Fiction", "Dystopian", "Science Fiction"],
+            "available": true
+        }
+    ]
 }
 
 
-console.log(mynavi[0], mynaivobj["naver"], mynaivobj.naver)
+
+
 
 
 
