8b8a78a91b5c10f1860faa1d7df40ef0e2256ca8
4thdraw <4thdraw2@gmail.com>
2024-06-15 11:15:08 +0900

    정렬과 오브젝트 접근방식

--- index.js
+++ index.js
@@ -6,13 +6,25 @@
 const myname = "방보영"
 let   myaccount = 100;
 myaccount += 1100 + 2000;
-console.log("금액확인",myaccount);
+//console.log("금액확인",myaccount);
 
 myaccount +="원";
 myaccount +=5000; 
 myaccount +=5000; 
 
-console.log("단위추가",myaccount, "데이터타입은? ",typeof myaccount);
+//console.log("단위추가",myaccount, "데이터타입은? ",typeof myaccount);
+
+const mynavi = ["네이버","다음","구글"];
+const mynaivobj = {
+    naver : "네이버",
+    daum : "다음",
+    goggle : "구글"
+}
+
+
+console.log(mynavi[0], mynaivobj["naver"], mynaivobj.naver)
+
+
 
 
 
