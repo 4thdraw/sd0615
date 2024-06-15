c20d5c8311e1f5b28b22efdf9a5039ae58a9bf96
4thdraw <4thdraw2@gmail.com>
2024-06-15 11:00:38 +0900

    바닐라 변수

---
+++ index.js
@@ -0,0 +1,19 @@
+// const | let | ver
+// const 선언과 동시 값 지정 절대불변 -> 상수선언
+// let 변수 -> 같은 이름 불가
+// ver 
+
+const myname = "방보영"
+let   myaccount = 100;
+myaccount += 1100 + 2000;
+console.log("금액확인",myaccount);
+
+myaccount +="원";
+myaccount +=5000; 
+myaccount +=5000; 
+
+console.log("단위추가",myaccount, "데이터타입은? ",typeof myaccount);
+
+
+
+
